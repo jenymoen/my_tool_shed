c65d36ab1dd4a4f5e69cc925db3d9256bef4a717
Jens Nymoen <jens.nymoen@gmail.com>
2025-05-14 09:03:29 +0200

    This is original when everything works

--- lib/models/tool.dart
+++ lib/models/tool.dart
@@ -18,23 +18,22 @@ class Tool {
   });
 
   Map<String, dynamic> toJson() => {
-    'id': id,
-    'name': name,
-    'imagePath': imagePath,
-    'isBorrowed': isBorrowed,
-    'returnDate': returnDate?.toIso8601String(),
-    'borrowedBy': borrowedBy,
-  };
+        'id': id,
+        'name': name,
+        'imagePath': imagePath,
+        'isBorrowed': isBorrowed,
+        'returnDate': returnDate?.toIso8601String(),
+        'borrowedBy': borrowedBy,
+      };
 
   factory Tool.fromJson(Map<String, dynamic> json) => Tool(
-    id: json['id'] as String,
-    name: json['name'] as String,
-    imagePath: json['imagePath'] as String?,
-    isBorrowed: json['isBorrowed'] as bool,
-    returnDate:
-        json['returnDate'] == null
+        id: json['id'] as String,
+        name: json['name'] as String,
+        imagePath: json['imagePath'] as String?,
+        isBorrowed: json['isBorrowed'] as bool,
+        returnDate: json['returnDate'] == null
             ? null
             : DateTime.parse(json['returnDate'] as String),
-    borrowedBy: json['borrowedBy'] as String,
-  );
+        borrowedBy: json['borrowedBy'] as String?,
+      );
 }
