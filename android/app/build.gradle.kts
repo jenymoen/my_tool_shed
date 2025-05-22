5e892248157ad7a92818e143c821917ec27d8ff3
Jens Nymoen <jens.nymoen@gmail.com>
2025-05-22 16:51:57 +0200

    Bump version code to 3 and version name to 2.0.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.lumin.my_tool_shed"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 3
+        versionName = "2.0.1"
     }
 
     signingConfigs {
